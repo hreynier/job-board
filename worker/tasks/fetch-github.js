c0b4290115ff666ad14c1253ebb0c538dce6b26d
Harvey Reynier <harveyreynier@gmail.com>
2020-09-30 12:38:20 +0200

    basic preliminary filtering algo for github jobs.

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -21,7 +21,7 @@ const baseURL = 'https://jobs.github.com/positions.json'
 
 
 async function fetchGithub() {
-
+    console.log('fetching github jobs.');
     /*  As base url only gives first page, we need to loop
         each page until we reach an empty page in order to
         fetch all of the possible jobs.
@@ -51,13 +51,31 @@ async function fetchGithub() {
     }
 
     console.log(`got ${allJobs.length} jobs in total.`);
-    const success = await setAsync('github', JSON.stringify(allJobs));
+
+    // Simple filtering algorithm
+    const jrJobs = allJobs.filter(job => {
+        const jobTitle = job.title.toLowerCase();
+        // algorithm logic
+        if(
+            jobTitle.includes('senior') || 
+            jobTitle.includes('sr.') || 
+            jobTitle.includes('manager') ||
+            jobTitle.includes('architect')
+        ) {
+            return false
+        }
+        return true
+    })
+    console.log(`filtered down to ${jrJobs.length} jr jobs in total.`);
+
+    // Send jobs to redis server.
+    const success = await setAsync('github', JSON.stringify(jrJobs));
 
     console.log({success});
 }
 
 // testing
-//fetchGithub();
+fetchGithub();
 
 // export for CronJobs.
 module.exports = fetchGithub;
