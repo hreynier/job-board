b0fd3bf742cbcb2227975914a19193317c3f5315
Harvey Reynier <harveyreynier@gmail.com>
2020-10-01 11:32:23 +0200

    added pagination to the jobs component.

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
+
 export default function Job({job}) {
     return (
         <Paper className={'job'}>
--- client/src/components/Jobs.js
+++ client/src/components/Jobs.js
@@ -2,27 +2,73 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
+import MobileStepper from '@material-ui/core/MobileStepper';
 import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+
 
 import Job from './Job';
 
 
 export default function Jobs({jobs}) {
 
-    console.log('jobs is', jobs[0]);
+    const numJobs = jobs.length;
+    const numPages = Math.ceil(numJobs / 50);
+
+    const [activeStep, setActiveStep] = React.useState(0);
+
+    const jobsOnPage = jobs.slice(activeStep * 50, (activeStep * 50) + 50);
+
+    // step = 0, show 0-49;
+    // step = 1, show 50-99; etc.
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+    
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
 
     return (
-        <div className="jobs">
-            <Typography variant="h4" component="h1">
-                Entry Level Software Jobs
-            </Typography>
-            {
-                jobs.map(
-                    job => <Job job={job} />
-                )
-            }
+      <div className="jobs">
+        <Typography variant="h4" component="h1">
+          Entry Level Software Jobs
+        </Typography>
 
+        <Typography variant="h6" component="h2">
+          Found {numJobs} Jobs 
+        </Typography>
+
+
+        {jobsOnPage.map(
+            (job, i) => (<Job job={job} />)
+        )}
+
+        <div>
+            Page {activeStep + 1} of {numPages}
         </div>
-    )
+
+        <MobileStepper
+          variant="progress"
+          steps={numPages}
+          position="static"
+          activeStep={activeStep}
+          nextButton={
+            <Button size="small" onClick={handleNext} disabled={activeStep === (numPages - 1)} >
+                Next 
+                <KeyboardArrowRight />
+            </Button>
+          }
+          backButton={
+            <Button size="small" onClick={handleBack} disabled={activeStep === 0} >
+                <KeyboardArrowLeft />
+                Back
+            </Button>
+            } 
+        />
+      </div>
+    );
 }
 
