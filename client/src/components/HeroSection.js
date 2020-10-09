6b0c7ee35597ad2235b786c7de5b6a848794b785
Harvey Reynier <harveyreynier@gmail.com>
2020-10-09 18:10:24 +0100

    Add hero image to hero section.

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-//import './HeroSection.css';
+import './HeroSection.css';
 
 export default function HeroSection() {
 
     return (
         <div className="hero-container">   
-            <img />
+            <img src="/assets/images/hero-working-table.jpg" />
             <h1>
                 Entry-level Software Jobs
             </h1>
