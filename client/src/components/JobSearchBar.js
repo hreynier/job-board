918af6a9bab1912d1d43b74923b355b2695ef061
Harvey Reynier <harveyreynier@gmail.com>
2020-10-14 11:45:10 +0100

    styled search bar - made rounder.

--- client/src/components/JobSearchBar.js
+++ client/src/components/JobSearchBar.js
@@ -4,15 +4,23 @@ import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
-import { Input } from '@material-ui/core';
 
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    searchContainer: {
+		padding: '5px 5px 5px 20px',
+		borderRadius: '30px'
+	}
+})
 
 
 export default function JobSearchBar({handleChange}) {  
 
+	const classes = useStyles();
 
 	return (
-	  <Paper className="search-bar-container">
+	  <Paper className={classes.searchContainer}>
 		  	<InputBase placeholder="Search Jobs..." inputProps={{ 'aria-label': 'search jobs'}} onChange={handleChange} />
 			
 			<IconButton type="submit" aria-label="search">
